30f6eb0f65d7d295e10ff2bcdba4cfc1324043d4
yellowbean <always.zhang@gmail>
2022-12-19 00:08:57 +0800

    add VAL

--- src/pyspecter/main.py
+++ src/pyspecter/main.py
@@ -15,6 +15,12 @@ class S(Enum):
     MVAL_IF=11
     SUB_MAP=12
     NTH_PATH=13
+    NONE_LIST=14
+    NONE_SET=15
+    NONE_TUPLE=16
+    #STAY=17
+    #STOP=18
+    VAL=19
 
 def query(d,p,debug=False):
     if debug:
@@ -24,6 +30,12 @@ def query(d,p,debug=False):
             return d
         case [S.MKEYS,*_r] if isinstance(d,dict):
             return query(list(d.keys()),_r)
+        case [S.ALL,*_r] :
+            if isinstance(d,list) or isinstance(d,range):
+                return [ query(_,_r) for _ in d ]
+            elif isinstance(d,dict):
+                _d = [ [k,v] for k,v in d.items()]
+                return query(_d,_r)
         case [(S.MKEY_IN,ks),*_r] if isinstance(d,dict):
             _d = {k:v for k,v in d.items() if k in ks}
             return query(_d,_r)
@@ -53,6 +65,21 @@ def query(d,p,debug=False):
             return query({k:v for k,v in d.items() if k in ks} ,_r)
         case [(S.NTH_PATH, *paths),*_r]:
             return [query(d,_r)[npth] for npth in paths]
+        case [S.NONE_LIST, *_r]:
+            if d is None:
+                return query([],_r)
+            return query(d,_r)
+        case [S.NONE_SET, *_r]:
+            if d is None:
+                return query(set(),_r)        
+            return query(d,_r)
+        case [S.NONE_TUPLE, *_r]:
+            if d is None:
+                return query((),_r)    
+            return query(d,_r)
+        case [S.VAL, *_r]:
+            sub_result = query(d,_r)
+            return [ [d, _] for _ in sub_result ]
         case [_h,*_r] if isinstance(d,dict):
             try:
                 return query(d[_h],_r)
