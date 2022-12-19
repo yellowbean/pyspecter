9665de0d0062621b081bc6097c0d7a5fcf9606ee
yellowbean <always.zhang@gmail>
2022-12-19 22:32:38 +0800

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyspecter",                     # This is the name of the package
-    version="0.0.2",                        # The initial release version
+    version="0.0.3",                        # The initial release version
     author="Xiaoyu Zhang",                     # Full name of the author
     description="A library to query nested data in Python",
     long_description=long_description,      # Long description read from the the readme file
